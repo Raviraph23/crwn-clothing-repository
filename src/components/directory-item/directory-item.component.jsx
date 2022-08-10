5cf7a6dbd1974bc68abcd94a7c1ebd2388baf1b2
Ravi N <ravifurious23@gmail.com>
2022-08-10 16:17:11 +0530

    Finished building allthe features of Crwn-Clothing website

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,25 @@
+import "./directory-item.styles.scss";
+import { useNavigate } from "react-router-dom";
+
+const DirectoyItem = ({ category }) => {
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+  return (
+    <div onClick={onNavigateHandler} className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default DirectoyItem;
